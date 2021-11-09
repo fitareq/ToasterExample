160fda465496b0e9cb15caff7a01663bd599bb21
fitareq <fitareq@gmail.com>
2021-11-09 17:43:40 +0600

    initial commit

--- ToasterLibrary/src/main/java/com/example/toasterlibrary/Toaster.java
+++ ToasterLibrary/src/main/java/com/example/toasterlibrary/Toaster.java
@@ -3,8 +3,39 @@ package com.example.toasterlibrary;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class Toaster {
-    public static void s(Context context, String message){
+
+
+    Context context;
+    RequestQueue requestQueue;
+    String url;
+    public Toaster(Context context, String url)
+    {
+        this.context = context;
+        this.url = url;
+        requestQueue = Volley.newRequestQueue(context);
+
+    }
+    public void s(String message){
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void geString(final VolleyCallBack callBack){
+        //String url = "https://jsonplaceholder.typicode.com/post";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,url, callBack::onSuccess, error -> callBack.onFailure(error.toString()) );
+        requestQueue.add(stringRequest);
+    }
+
+
+    public interface VolleyCallBack{
+        void onFailure(String msg);
+        void onSuccess(String response);
+
     }
 }
--- app/src/main/java/com/example/toasterexample/MainActivity.java
+++ app/src/main/java/com/example/toasterexample/MainActivity.java
@@ -8,12 +8,26 @@ import com.example.toasterlibrary.Toaster;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String url = "https://jsonplaceholder.typicode.com/posts";
+        Toaster toaster = new Toaster(this, url);
+        toaster.geString(new Toaster.VolleyCallBack() {
+            @Override
+            public void onSuccess(String result) {
+                toaster.s(result);
+            }
 
-        Toaster.s(this, "hello");
+            @Override
+            public void onFailure(String msg) {
+                toaster.s(msg);
+            }
+        });
+
+
 
     }
 }
